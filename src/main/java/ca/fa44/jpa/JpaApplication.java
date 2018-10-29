4e89c1fc91a31823b45b1d36b39b760b304d417b
fa44 <fanilo111222@gmail.com>
2018-10-28 23:59:19 -0400

    Init travis

---
+++ src/main/java/ca/fa44/jpa/JpaApplication.java
@@ -0,0 +1,12 @@
+package ca.fa44.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApplication.class, args);
+	}
+}
